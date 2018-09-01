7be982c3d4891ecaf1b1308f17c3a1250b877bde
ggoonn <@>
2018-09-01 17:46:20 +0900

    テスト時にオプション指定できるようにした

--- test.sh
+++ test.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # expr言語は現在の言語仕様上、
 # 標準出力に出力する手段がないため、計算結果を戻り値として返している
+#
+# TESTOPT="-O" ./test.sh とかでテスト時の追加のオプションを指定可能
 
 EXEFILE="exprc"
 TESTDIR="test"
@@ -12,7 +14,7 @@ success_num=0
 for testcase in ${testcaselist}; do
 	expected=$(grep "^##return " ${testcase} | sed "s/^##return //")
 
-	./${EXEFILE} ${testcase} -print-llvm | lli -force-interpreter
+	./${EXEFILE} ${testcase} -f ${TESTOPT} | lli -force-interpreter
 	ret=$?
 
 	if [ ${ret} -eq ${expected} ]; then
