487589126899dd7e4b9d86c993c88d178ba61dc7
ggoonn <@>
2018-09-02 20:40:04 +0900

    標準出力への出力のテストをできるようにした

--- test.sh
+++ test.sh
@@ -4,6 +4,9 @@
 # *出力可能になったため、順次標準出力の確認に変更する*
 #
 # TESTOPT="-O" ./test.sh とかでテスト時の追加のオプションを指定可能
+#
+# `##return 0`   戻り値の期待値
+# `##printn aaa` 標準出力への出力の期待値(改行付き) エスケープシーケンスは使えない
 
 EXEFILE="exparrc"
 TESTDIR="test"
@@ -11,22 +14,39 @@ TESTDIR="test"
 test -f ./${EXEFILE} || { echo "not found './${EXEFILE}'"; exit 1; }
 
 testcaselist=$(ls ${TESTDIR}/*.ea)
+test_num=0
 success_num=0
 for testcase in ${testcaselist}; do
-	expected=$(grep "^##return " ${testcase} | sed "s/^##return //")
+	expected_r=$(grep "^##return " ${testcase} | sed "s/^##return //")
+	expected_p=$(grep "^##printn " ${testcase} | sed "s/^##printn //")
 
-	./${EXEFILE} ${testcase} -f ${TESTOPT} | lli -force-interpreter
-	ret=$?
+	result_pn=$(./${EXEFILE} ${testcase} -f ${TESTOPT} | lli -force-interpreter)
+	result_ret=$?
 
-	if [ ${ret} -eq ${expected} ]; then
-		printf "\e[32mSuccess:\e[m"
-		success_num=$((success_num + 1))
-	else
-		printf "\e[31mFailure:  (ret=${ret} exp=${expected})\e[m"
+	# 戻り値のテスト
+	if [ -n "${expected_r}" ]; then
+		test_num=$((test_num + 1))
+		if [ ${result_ret} -eq ${expected_r} ]; then
+			success_num=$((success_num + 1))
+			printf "\e[32mSuccess:  (return)\e[m"
+		else
+			printf "\e[31mFailure:  (return ${result_ret}:${expected_r})\e[m"
+		fi
+		echo "  [${testcase}]"
 	fi
-	echo "  [${testcase}]"
+
+	# 標準出力への出力のテスト
+	if [ -n "${expected_p}" ]; then
+		test_num=$((test_num + 1))
+		if [ "${result_pn}" = "${expected_p}" ]; then
+			success_num=$((success_num + 1))
+			printf "\e[32mSuccess:  (printn)\e[m"
+		else
+			printf "\e[31mFailure:  (printn ${result_pn}:${expected_p})\e[m"
+		fi
+		echo "  [${testcase}]"
+	fi
 done
 
-test_num=$(echo ${testcaselist} | wc -w)
 echo "${success_num} / ${test_num}"
 
