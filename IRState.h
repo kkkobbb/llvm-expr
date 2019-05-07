829b19de3f12ec47d70b7c4fa8d5ccfabb889466
koba <@>
2019-05-08 01:39:36 +0900

    dynamic_castを使用しないようにした
    代わりにstatic_assertでコンパイル時に確認するようにした
    (実行ファイルのサイズは増えるかも)

--- IRState.h
+++ IRState.h
@@ -7,6 +7,7 @@
 #include <string>
 #include <memory>
 #include <vector>
+#include <type_traits>
 
 
 namespace expr {
@@ -37,30 +38,33 @@ namespace expr {
 		llvm::GlobalVariable *getGlobalString(const char *str);
 		llvm::Value *getVariable(const std::string *name);
 
-		// ブロックの階層をスタックに保存してIR生成する
+		// (必要があれば)ブロックの階層をスタックに保存してIR生成する
 		//
-		// テンプレートにNode以外の型を指定した場合、その型のgetValue()を使用する
+		// テンプレートTにNode以外の型を指定した場合、その型のgetValue()を使用する
 		//   (基底クラスのgetValue()を実行したい場合用)
-		// T Node*の派生クラスであること (そうでない場合、T=Nodeとみなす)
-		//   かつnodeに渡した型の基底クラスであること
+		//
+		// T Nodeの派生クラスであること
+		//   かつnodeの型Uの基底クラスであること
+		// U nodeの型であり、明示的に指定しないこと
+		// (TもUもNodeの派生クラス(Node型含む)であること)
 		//
 		// node IR生成するノード
 		// needNewBlock 新しい階層のブロックでIR生成する場合、真
-		template<class T=Node>
-		llvm::Value *createValueInBlock(Node *node, bool needNewBlock=false)
+		template<class T=Node, class U>
+		llvm::Value *createValueInBlock(U *node, bool needNewBlock=false)
 		{
+			// TがNodeの派生クラスであること (そうでない場合、コンパイル時エラー)
+			static_assert(std::is_base_of<Node, T>::value, "'T' is bad class");
+			// TがUの基底クラスであること (そうでない場合、コンパイル時エラー)
+			static_assert(std::is_base_of<T, U>::value, "'T' is not base of 'U'");
+
 			if (node == nullptr)
 				return nullptr;
 
 			if (needNewBlock)
 				pushBlock(builder->GetInsertBlock());
 
-			llvm::Value *value;
-			// FIXME dynamic_cast を使っているので実行コストが大きい
-			if(dynamic_cast<T *>(node) != nullptr)
-				value = getValue_((T *)node);
-			else
-				value = getValue_(node);
+			const auto value = getValue_((T *)node);
 
 			if (needNewBlock)
 				popBlock();
@@ -81,11 +85,12 @@ namespace expr {
 		template <class T>
 		llvm::Value *getValue_(T *node)
 		{
-			// 基底クラスのgetValue()を実行する
+			// T型のgetValue()を実行する
 			return node->T::getValue(*this);
 		}
 		llvm::Value *getValue_(Node *node)
 		{
+			// nodeの実体の型のgetValue()を実行する
 			return node->getValue(*this);
 		}
 	};
