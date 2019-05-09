545d3c39224f28d65c77aaa9caff3179fb7ac038
koba <@>
2019-05-09 15:51:43 +0900

    変数名修正

--- main.cpp
+++ main.cpp
@@ -87,10 +87,10 @@ int main(int argc, char *argv[])
 	// IR生成
 	IRGenerator irGen;
 	const bool irGenerated = irGen.generate(*ast.get());
-	const auto m = irGen.get();
+	const auto module = irGen.get();
 	// 入力ファイル名を出力用に設定
 	const auto ifname = llvm::sys::path::filename(InputFilename);
-	m->setSourceFileName(ifname);
+	module->setSourceFileName(ifname);
 
 	// 生成に失敗した場合、終了
 	if (!irGenerated) {
@@ -98,7 +98,7 @@ int main(int argc, char *argv[])
 			return 1;
 		// llvm IR 表示の指定があれば、エラーでもIRは表示する
 		llvm::outs() << "\n\n";
-		m->print(llvm::outs(), nullptr);
+		module->print(llvm::outs(), nullptr);
 		llvm::outs() << "\n\n";
 		return 1;
 	}
@@ -106,13 +106,13 @@ int main(int argc, char *argv[])
 	if (Optim) {
 		// 最適化
 		OptimPass opp;
-		if (!opp.run(*m.get()))
+		if (!opp.run(*module.get()))
 			return 1;
 	}
 
 	if (PrintLlvm) {
 		// llvm IR 表示
-		m->print(llvm::outs(), nullptr);
+		module->print(llvm::outs(), nullptr);
 		llvm::outs() << "\n";
 		return 0;
 	}
@@ -129,7 +129,7 @@ int main(int argc, char *argv[])
 	OutputPassFactory opf;
 	const auto op = opf.create(FileType);
 	if (op)
-		op->run(*m.get(), ofname);
+		op->run(*module.get(), ofname);
 
 	return 0;
 }
