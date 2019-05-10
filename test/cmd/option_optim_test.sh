959860a0e1008786c9b244d27b7c6873c3d959e5
koba <@>
2019-05-10 22:12:08 +0900

    最適化オプション実行のテストを追加

---
+++ test/cmd/option_optim_test.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+# 最適化オプションの確認
+#
+# 成功時、0を返す
+#
+
+######################### 準備 #########################
+DEFAULT_EXE="./exparrc"
+TESTDIR="$(dirname $0)"
+. ${TESTDIR}/run_init.sh
+
+
+# テストとして実行するコマンド
+testcase()
+{
+	# 試験用ソースコード生成
+	srcfname="test.ea"
+	printf "a<-4;\nb<-5;\nc<-a+b;" > ${srcfname}
+
+	# 実行
+	${TEST_EXE} -O ${srcfname}
+	ret=$?
+
+	# 戻り値が0でない場合、失敗
+	if [ "${ret}" -ne 0 ]; then
+		echo "bad return (${ret})"
+		return 1
+	fi
+
+	# ファイルを生成していない場合、失敗
+	if [ ! -f "a.s" ]; then
+		echo "no asm file"
+		return 1
+	fi
+
+	# 生成ファイルの実行
+	gcc a.s -o output
+	./output
+	output_ret=$?
+
+	# 生成ファイルをコンパイルしてできた実行ファイルの戻り値
+	if [ "${output_ret}" -ne 9 ]; then
+		echo "bad generated exefile return (${output_ret})"
+		return 1
+	fi
+
+	return 0
+}
+
+
+######################### 実行 #########################
+
+run_test testcase
+
